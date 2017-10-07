0880ebbc12ecd9c49548943ce3b56528f1fb3004
Sanketbhangale <sanketvbhangale@gmail.com>
2017-10-07 23:38:10 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,15 @@
+rvm install ruby-2.3.0
+rvm --default use ruby-2.3.0
+gem install bundler --no-rdoc --no-ri
+sudo apt-get install -y nodejs &&
+sudo ln -sf /usr/bin/nodejs /usr/local/bin/node
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+
+# Add our APT repository
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+
+# Install Passenger + Nginx
+sudo apt-get install -y nginx-extras passenger
