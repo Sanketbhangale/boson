4ed094a997d924c423692b8a75535e2585585614
Sanketbhangale <sanketvbhangale@gmail.com>
2018-02-13 00:36:59 +0100

    Update install2.sh
    Xenial instead of trusty

--- install2.sh
+++ install2.sh
@@ -11,7 +11,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CA
 sudo apt-get install -y apt-transport-https ca-certificates
 
 # Add our APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
 
 # Install Passenger + Nginx
