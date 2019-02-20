d80054618c6c31e0ed18e2dbdb681c4695189ff1
Sanketbhangale <sanketvbhangale@gmail.com>
2019-02-20 18:03:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y curl gnupg build-essential
 
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | sudo bash -s stable
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDBcurl -sSL https://get.rvm.io | sudo bash -s stable
 echo 'Who Am I?'
 sudo usermod -a -G rvm `whoami`
 
